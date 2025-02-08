d82317c81e2b1c75f53bcd93ce78663c5dcc8062
londonho <avh95246@uga.edu>
2025-02-07 20:43:54 -0500

    Initial Commit

---
+++ Hoppin/HoppinApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoppinApp.swift
+//  Hoppin
+//
+//  Created by london ho on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct HoppinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HoppinTests/HoppinTests.swift
@@ -0,0 +1,17 @@
+//
+//  HoppinTests.swift
+//  HoppinTests
+//
+//  Created by london ho on 2/7/25.
+//
+
+import Testing
+@testable import Hoppin
+
+struct HoppinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
